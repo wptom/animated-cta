760c9f61ba55fc453fc8bf050cb55f3ea176733d
wptom.com <tom@wptom.com>
2022-06-19 17:46:47 +0200

    Styling and functions.

--- src/js/modules/animated-cta.js
+++ src/js/modules/animated-cta.js
@@ -1,32 +1,47 @@
 class AnimatedCTA {
-    constructor() {
+    constructor(id) {
+        this.id = id;
+        this.body = window.document.querySelector('body');
+        this.cta = document.getElementById(id);
         this.initEvents();
     }
 
+    getPositionTop() {
+        return Math.floor(this.cta.getBoundingClientRect().top);
+    }
+
+    getPositionBottom() {
+        return Math.floor(this.cta.getBoundingClientRect().bottom);
+    }
+
+    getViewportDims(){
+        return {
+            viewportWidth: window.innerWidth,
+            viewportHeight: window.innerHeight
+        }
+    }
+
     initEvents() {
-        let cta = document.getElementById('animated-cta');
-
-        const getPosition = () => {
-            return Math.floor(cta.getBoundingClientRect().top);
-        }
-
-        const getViewportDims = () => {
-            return {
-                viewportWidth: window.innerWidth,
-                viewportHeight: window.innerHeight
-            }
-        }
+        // BODY tag has to have class page-has-animated-cta in order to add Event listener.
+        if (!this.body.classList.contains('page-has-animated-cta')) { return false; }
 
         document.addEventListener('scroll', (e) => {
-            let ctaOffsetTop = getPosition();
-            let viewportHeight = getViewportDims().viewportHeight;
+            e.preventDefault();
 
-            if (ctaOffsetTop >= (viewportHeight * 0.33) && ctaOffsetTop <= (viewportHeight * 0.66)) {
-                cta.classList.add('highlighted');
+            let ctaOffsetTop = this.getPositionTop();
+            let ctaOffsetBottom = this.getPositionBottom();
+            let viewportHeight = this.getViewportDims().viewportHeight;
+
+            if (ctaOffsetTop <= (viewportHeight * 0.66) && ctaOffsetTop >= 0) {
+                console.log('active', this.id, document.querySelector('body').classList);
+                this.cta.classList.add('animated-cta-highlighted');
+                this.body.classList.add('animated-cta-overlay');
             } else {
-                cta.classList.remove('highlighted');
+                console.log('inactive', this.id, document.querySelector('body').classList);
+                this.cta.classList.remove('animated-cta-highlighted');
+                this.body.classList.remove('animated-cta-overlay');
             }
-        })
+        });
     }
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import AnimatedCTA from './modules/animated-cta.js';
 
 window.addEventListener('load', () => {
-    new AnimatedCTA();
+    new AnimatedCTA('animated-cta1');
+    new AnimatedCTA('animated-cta2');
 });
